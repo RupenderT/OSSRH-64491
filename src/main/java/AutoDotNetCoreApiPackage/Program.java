1e2b7b712b2f225d66e24cfaea1d938919fa58fc
RupenderT <35565641+RupenderT@users.noreply.github.com>
2021-02-14 13:55:33 +0530

    jhgj

--- src/main/java/AutoDotNetCoreApiPackage/Program.java
+++ src/main/java/AutoDotNetCoreApiPackage/Program.java
@@ -1,12 +1,11 @@
 package AutoDotNetCoreApiPackage;
 
-import java.io.PrintStream;
 import java.util.Arrays;
 
 public class Program {
 
 	public static void main(String[] args) {
-		 String api="http://localhost:53106/api/myapi/";
+		 String api="http://localhost:5000/api/myapi/";
 		 AutoDotNetCoreApiBuilder builder=new AutoDotNetCoreApiBuilder(api, "");
          AutoDotNetCoreApiTable<Employee> employee=builder.Table("Employees", Employee[].class);
          Employee[] arr=employee.GetResult();
